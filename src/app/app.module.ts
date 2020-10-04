1d084ac788599e2b455fd1d75fa74ce7fa2c2d6f
Matthew Rungwe <matthewrungwe@gmail.com>
2020-10-04 16:10:29 +0200

    Create all basic components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AdvertComponent } from './advert/advert.component';
+import { ShowroomComponent } from './showroom/showroom.component';
+import { RoomComponent } from './room/room.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AdvertComponent,
+    ShowroomComponent,
+    RoomComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
